c26bba4f4cd95f9dd55a7bf073165a32237d5d8f
pkrzekotowski <piotr.krzekotowski79@gmail.com>
2024-06-20 19:03:01 +0200

    completed exercise 2.2

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,12 +1,29 @@
+const Header = ({ course }) => <h2>{course.name}</h2>
+
+const Note = ({ name, exercises }) => <p>{name} {exercises}</p>
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((acc, part) =>
+    acc += part.exercises, 0)
+  return (
+    <div>
+      <b>total of {sum} exercises</b>
+    </div>
+  )
+}
+
 const Course = ({ course }) => {
   return (
     <div>
-      <h2>{course.name}</h2>
+      <Header course={course} />
       {course.parts.map(part =>
-        <p key={part.id}>
-          {part.name} {part.exercises}
-        </p>
+        <Note
+        key={part.id}
+        name={part.name}
+        exercises={part.exercises}
+        />
       )}
+      <Total course={course} />
     </div>
   )
 }
