0cd1cf36a053b9561ac0faf2f9824c213e648a09
pkrzekotowski <piotr.krzekotowski79@gmail.com>
2024-06-22 18:34:43 +0200

    completed exercises 2..9*

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState(persons)
 
   const handleNameChange = (e) => {
     console.log(e.target.value)
@@ -13,6 +20,12 @@ const App = () => {
   const handleNumberChange = (e) => {
     console.log(e.target.value)
     setNewNumber(e.target.value)
+  }
+
+  const handleFiltering = (e) => {
+    const keyword = e.target.value.toLowerCase()
+    setFilter(keyword)
+    setFilteredPersons(persons.filter(person => person.name.toLowerCase().includes(keyword)))
   }
 
   const isContactInPhonebook = () =>
@@ -39,6 +52,13 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with: <input
+            value={filter}
+            onChange={handleFiltering}
+          />
+        </div>
+      <h2>add a new</h2>
       <form onSubmit={addNewPerson}>
         <div>
           name: <input
@@ -57,7 +77,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
+      {filteredPersons.map(person =>
       <div key={person.name}>{person.name} {person.number}</div>
       )}
     </div>
