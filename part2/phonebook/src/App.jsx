a74ed2ef76ddf26085e4d09b041db4aa46ac2d9a
pkrzekotowski <piotr.krzekotowski79@gmail.com>
2024-06-26 13:47:59 +0200

    minor code fixes

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Filter = ({ filter, handleFiltering }) => (
-  <div>
-      filter shown with: <input value={filter} onChange={handleFiltering} />
-  </div>
-)
+const Filter = ({ filter, handleFiltering }) => <div>filter shown with: <input value={filter} onChange={handleFiltering} /> </div>
 
 const PersonForm = ({ addNewPerson, newPerson, handleFormChange }) => (
     <form onSubmit={addNewPerson}>
@@ -21,16 +17,14 @@ const PersonForm = ({ addNewPerson, newPerson, handleFormChange }) => (
     </form>
 )
 
-const Persons = ({ persons, keyword }) => {
-  return (
-    <div>
-      {persons
-          .filter(person => person.name.toLowerCase().includes(keyword.toLowerCase()))
-          .map(person => <div key={person.id}>{person.name} {person.number}</div>)
-        }
+const Persons = ({ persons, keyword }) => (
+  <div>
+    {persons
+        .filter(person => person.name.toLowerCase().includes(keyword.toLowerCase()))
+        .map(person => <div key={person.id}>{person.name} {person.number}</div>)
+      }
   </div>
-  )
-}
+)
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -45,18 +39,17 @@ const App = () => {
       })
     }, [])
 
-  const handleFormChange = (event, field) =>
-    setNewPerson({...newPerson, [field]: event.target.value})
-
   useEffect(() => {
     console.log(`Current name: ${newPerson.name}, Current number: ${newPerson.number}`)
   }, [newPerson])
 
-  const handleFiltering = (event) =>
-    setFilter(event.target.value)
+  const handleFormChange = (e, field) =>
+    setNewPerson({...newPerson, [field]: e.target.value})
 
-  const addNewPerson = (event) => {
-    event.preventDefault()
+  const handleFiltering = (e) => setFilter(e.target.value)
+
+  const addNewPerson = (e) => {
+    e.preventDefault()
     const { name, number } = newPerson
 
     if (persons.some(person => person.name.toLowerCase() === newPerson.name.toLowerCase())) {
@@ -77,10 +70,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter
-        filter={filter}
-        handleFiltering={handleFiltering}
-      />
+      <Filter filter={filter} handleFiltering={handleFiltering} />
       <h3>add a new</h3>
       <PersonForm addNewPerson={addNewPerson} newPerson={newPerson} handleFormChange={handleFormChange} />
       <h2>Numbers</h2>
